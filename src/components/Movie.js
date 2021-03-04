9974f482946848fa1dd2bd0c4e86f1780fe6a460
ilham rizky <ilhamrizkyakbar@gmail.com>
2021-03-04 16:46:45 +0700

    load first page of movie with the poster image, title, not fixed release date, and vote average

---
+++ src/components/Movie.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const IMAGE_API = "https://image.tmdb.org/t/p/w1280";
+
+const Movie = ({ title, poster_path, release_date, vote_average }) => (
+    <div className="movie">
+        <h4 className="movie-rating">{vote_average}</h4>
+        <img src={IMAGE_API + poster_path} alt={title} />
+        <div className="movie-info">
+            <h3>{title}</h3>
+            <span>{release_date}</span>
+        </div>
+
+    </div>
+)
+
+export default Movie;
